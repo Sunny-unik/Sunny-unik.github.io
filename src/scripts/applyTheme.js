42dd2980fcbfc6fb86ae6f371fa9714d6fd13851
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2024-02-16 00:57:41 +0530

    feat: add theme toggle checkbox

--- src/scripts/applyTheme.js
+++ src/scripts/applyTheme.js
@@ -1,7 +1,18 @@
 export default function () {
+  const toggleInput = document.querySelector(".toggle-wrapper input");
   const darkPreference = window.matchMedia(
     "(prefers-color-scheme: dark)"
   ).matches;
 
-  darkPreference && document.documentElement.setAttribute("data-theme", "dark");
+  if (darkPreference) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    toggleInput.checked = true;
+  }
+
+  toggleInput.addEventListener("change", (event) => {
+    document.documentElement.setAttribute(
+      "data-theme",
+      event.currentTarget.checked ? "dark" : "light"
+    );
+  });
 }
