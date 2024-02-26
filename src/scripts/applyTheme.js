8aa418c9bd42cc5bcb3375b36fd74581763e1d4e
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2024-02-26 11:05:58 +0530

    feat: manage dark mode state using localstorage

--- src/scripts/applyTheme.js
+++ src/scripts/applyTheme.js
@@ -3,15 +3,24 @@ export default function () {
   const darkPreference = window.matchMedia(
     "(prefers-color-scheme: dark)"
   ).matches;
+  const themePreset = localStorage.getItem("theme");
 
-  if (darkPreference) {
+  if (themePreset) {
+    document.documentElement.setAttribute("data-theme", themePreset);
+    toggleInput.checked = themePreset === "dark";
+  } else if (darkPreference) {
     document.documentElement.setAttribute("data-theme", "dark");
     toggleInput.checked = true;
+    localStorage.setItem("theme", "dark");
   }
 
   toggleInput.addEventListener("change", (event) => {
     document.documentElement.setAttribute(
       "data-theme",
+      event.currentTarget.checked ? "dark" : "light"
+    );
+    localStorage.setItem(
+      "theme",
       event.currentTarget.checked ? "dark" : "light"
     );
   });
