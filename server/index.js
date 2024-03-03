9c3e8b628d35f522958ead9997130a9d4cc94696
Sunny Gandhwani <65410458+Sunny-unik@users.noreply.github.com>
2024-03-03 15:34:37 +0530

    feat: implement CORS checks in server

--- server/index.js
+++ server/index.js
@@ -6,7 +6,23 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  if (req.method === "POST" && req.url === "/send-email") {
+  const requestOrigin = req.headers.origin;
+  const corsHeaders = {
+    "Access-Control-Allow-Origin": requestOrigin,
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+    "Access-Control-Allow-Headers": "Content-Type",
+  };
+
+  if (req.method === "OPTIONS") {
+    // Handle preflight request
+    if (!JSON.parse(process.env.ALLOWED_ORIGINS).includes(requestOrigin)) {
+      console.log(`Request from unallowed origin: ${requestOrigin}`);
+      res.writeHead(403, { "Content-Type": "text/plain" });
+      return res.end("Forbidden: Request from unallowed origin");
+    }
+    res.writeHead(200, corsHeaders);
+    res.end("ended");
+  } else if (req.method === "POST" && req.url === "/send-email") {
     let body = "";
     req.on("data", (chunk) => (body += chunk.toString()));
 
@@ -23,12 +39,12 @@ const server = http.createServer((req, res) => {
       sendMail(mailOptions)
         .then((info) => {
           console.log("Email sent: " + info.response);
-          res.writeHead(200, { "Content-Type": "text/plain" });
+          res.writeHead(200, { "Content-Type": "text/plain", ...corsHeaders });
           res.end("Email sent successfully");
         })
         .catch((error) => {
           console.error(error);
-          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.writeHead(500, { "Content-Type": "text/plain", ...corsHeaders });
           res.end("Error sending email");
         });
     });
@@ -36,7 +52,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("All Good :-)");
   } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.writeHead(404, { "Content-Type": "text/plain", ...corsHeaders });
     res.end("Not Found");
   }
 });
