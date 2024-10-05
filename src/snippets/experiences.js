546b7defee1443f2c03ec1d6fad9ab5a66d80afb
Sunny Gandhwani <sunny.gandhwani@pabbly.com>
2024-10-05 20:13:53 +0530

    update new experiences

--- src/snippets/experiences.js
+++ src/snippets/experiences.js
@@ -11,9 +11,9 @@ export default function addExperiences() {
       <i class="fa fa-calendar"></i>{{date}}
     </div>
     <h4>
-      <i class="fa fa-flag"></i>{{company}} – {{location}}
+      <i class="fa fa-flag"></i>{{company}} – {{location}} 
     </h4>
-    <p class="experienceDetails">
+        <p class="experienceDetails">
       {{description}}
     </p>
   </div>
@@ -21,20 +21,60 @@ export default function addExperiences() {
 `;
   const experiences = [
     {
+      title: "Software Developer",
+      description: `• Led code reviews and guided junior developers.
+      <br>• Streamlined processes with automation tools and real-time webhooks.
+      <br>• Optimized I/O operations using asynchronous programming.
+      <br>• Handled third-party integrations and APIs for smooth system connectivity.
+      <br>• Managed MongoDB and Redis, ensuring data integrity.`,
+      date: "June 2024 - Present",
+      jobType: "Full-time",
+      locationType: "Onsite",
+      company: "Pabbly",
+      location: "Bhopal, Madhya Pradesh, India",
+    },
+    {
+      title: "Freelance Full Stack Developer",
+      description: `<b>Worked on Next.js Web Applications:</b><br>
+      • Built dynamic web apps using server-side rendering (SSR) and static site generation (SSG).<br>
+      • Integrated third-party APIs & CMS to developed dynamic, heavily customizable and responsive user interfaces.<br>
+      <b>Developed Pdf Word Finder Chrome Extensions</b><br>
+      • Developed a Chrome extension for advanced word search in PDFs using Express-PDF.<br>
+      • Created efficient search algorithms and user-friendly interfaces.<br>
+      <b>Developed WhatsApp Web Extension</b><br>
+      • Built a WhatsApp Web extension for automated messaging using whatsapp-web.js and Baileys.<br>
+      • Focused on secure data handling and enhanced user interaction.`,
+      date: "Nov 2023 - April 2024",
+      jobType: "Part-time",
+      locationType: "Remote",
+      company: "Self Employed",
+      location: "Suwasra, Madhya Pradesh, India",
+    },
+    {
       title: "Web Developer",
-      description:
-        "As a web developer at Spicegems, my primary responsibility is to write TypeScript scripts and develop complex logic for building dynamic Shopify apps with versatile customization features. Effective collaboration with the team is essential to integrate the scripts seamlessly and ensure a high-quality user experience.",
+      description: `• As a Web Developer in this company, my responsibility is to write scripts (Typescript) to build
+        dynamic Shopify apps.<br>
+        • Implemented cross-browser compatibility strategies to ensure smooth functioning of web apps
+        across different browsers.<br>
+        • These apps offer extensive customization options to users and seamlessly adapt to different
+        themes (800+ themes).<br>
+        • These apps are used by hundreds of businesses & rated 4.8+ stars on Shopify app store.
+        • Also, my responsibilities include writing test cases using Jest.`,
       date: "Jan 2022 - July 2023",
+      jobType: "Full-time",
+      locationType: "Onsite",
       company: "Spicetech IT Solutions PVT LTD",
-      location: "Kota, (Rajasthan)",
+      location: "Kota, Rajasthan, India",
     },
     {
       title: "Full-stack Developer",
       description:
         "During my tenure at Grras Solutions, I gained valuable expertise in creating single-page applications using the MERN stack (MongoDB, Express.js, React.js, and Node.js). This comprehensive training empowered me to develop dynamic and interactive web applications with seamless frontend and backend integration. Additionally, I learned how to deploy these applications on cloud services, ensuring scalability and accessibility. This hands-on experience has equipped me to tackle real-world web development challenges and deliver cutting-edge solutions that align with industry best practices.",
       date: "May 2021 - Aug 2021",
+      jobType: "Apprenticeship",
+      locationType: "Remote",
       company: "Grras Solutions PVT LTD",
-      location: "Jaipur, (Rajasthan)",
+      location: "Jaipur, Rajasthan, India",
     },
   ];
   const finalizedExp = experiences.map((experience) =>
