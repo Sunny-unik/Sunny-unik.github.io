f3f14bddf2f88e6d6e3c27dac6970d79ed09599d
Sunny Gandhwani <sunny.gandhwani@pabbly.com>
2025-02-02 23:35:25 +0530

    add .env.example, routes & controllers

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const http = require("http");
 const dotenv = require("dotenv");
-const sendMail = require("./services/mailer");
+const { sendMailController } = require("./controllers/sendMailController");
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+const allowedPostRoutes = ["/send-email", "/send-chat"];
 
 const server = http.createServer((req, res) => {
   const requestOrigin = req.headers.origin;
@@ -16,37 +17,24 @@ const server = http.createServer((req, res) => {
   if (req.method === "OPTIONS") {
     // Handle preflight request
     if (!JSON.parse(process.env.ALLOWED_ORIGINS).includes(requestOrigin)) {
-      console.log(`Request from unallowed origin: ${requestOrigin}`);
+      console.log(`Request from forbidden origin: ${requestOrigin}`);
       res.writeHead(403, { "Content-Type": "text/plain" });
-      return res.end("Forbidden: Request from unallowed origin");
+      return res.end("Forbidden: Request from forbidden origin");
     }
     res.writeHead(200, corsHeaders);
     res.end("ended");
-  } else if (req.method === "POST" && req.url === "/send-email") {
+  } else if (req.method === "POST" && allowedPostRoutes.includes(req.url)) {
     let body = "";
     req.on("data", (chunk) => (body += chunk.toString()));
 
     req.on("end", () => {
-      const { name, email, subject, message } = JSON.parse(body);
-      const mailOptions = {
-        from: process.env.MAIL_ADDRESS,
-        appPassword: process.env.APP_PASSWORD,
-        to: process.env.MAIL_ADDRESS,
-        subject: subject,
-        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-      };
-
-      sendMail(mailOptions)
-        .then((info) => {
-          console.log("Email sent: " + info.response);
-          res.writeHead(200, { "Content-Type": "text/plain", ...corsHeaders });
-          res.end("Email sent successfully");
-        })
-        .catch((error) => {
-          console.error(error);
-          res.writeHead(500, { "Content-Type": "text/plain", ...corsHeaders });
-          res.end("Error sending email");
-        });
+      const data = JSON.parse(body);
+      const parameters = [corsHeaders, data, res];
+      if (req.url === "/send-email") {
+        sendMailController(...parameters);
+      } else if (req.url === "/send-chat") {
+        sendMailController(...parameters);
+      }
     });
   } else if (req.method === "GET") {
     res.writeHead(200, { "Content-Type": "text/plain" });
